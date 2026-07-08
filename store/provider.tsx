7f1c893f7e60eba70e3e0136e78a7db883c512e2
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2026-07-08 05:33:00 +0300

    feat: add ReduxProvider component for Redux store integration with persistence

---
+++ store/provider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./index";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function ReduxProvider({ children }: Props) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+}
